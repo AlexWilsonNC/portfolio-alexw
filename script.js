ab0b7146349ef254f4def28451d8ad8d4cfecab8
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-09-06 15:11:17 -0400

    new backdrop, side nav, multi page scroll

---
+++ script.js
@@ -0,0 +1,13 @@
+// unhide up icon outside of home
+
+
+// window.onload = function disableScroll() {
+//     // Get the current page scroll position
+//     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  
+//         // if any scroll is attempted, set this to the previous value
+//         window.onscroll = function() {
+//             window.scrollTo(scrollLeft, scrollTop);
+//         };
+// }
