9ad225cb4f776e5da1a5f4076334a9f507d0235b
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-09-06 21:03:21 -0400

    contact page and works

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 //     // Get the current page scroll position
 //     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 //     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-  
+
 //         // if any scroll is attempted, set this to the previous value
 //         window.onscroll = function() {
 //             window.scrollTo(scrollLeft, scrollTop);
